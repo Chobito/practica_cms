d830c51c9ebd4ee3fd229dd2abd6d39b1659e302
Pedro <ff7maniac@gmail.com>
2017-03-20 09:40:00 +0100

    commit del dia

---
+++ pandoc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Pedro Romero Aguado
+# isx46874983
+# 16/03/2017
+# Script per transformar documents markdown a xhtml 
+# Standalone: Document with a proper header and footer
+# Read: Document Type input
+# write: Document Type output
+# Output: Name of desired output file
+# At the end of script put the input file name
+
+pandoc \
+	--standalone \
+	--read markdown \
+	--write html  \
+	--output attach.html \
+	attach.md
+
+pandoc \
+	--standalone \
+	--read markdown \
+	--write html  \
+	--output commit.html \
+	commit.md
+
+pandoc \
+	--standalone \
+	--read markdown \
+	--write html  \
+	--output exec.html \
+	exec.md
